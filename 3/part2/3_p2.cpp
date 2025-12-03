da3f595877374c875a330c06b3cfb59c4480229d
danya-chernikov <dchernik@proton.me>
2025-12-03 20:47:56 +0100

    The second part of the third day puzzle was solved

--- 3/part2/3_p2.cpp
+++ 3/part2/3_p2.cpp
@@ -49,8 +49,6 @@ int main(int argc, char **argv)
 		skip = 0;
 		rest = digits.size();
 
-		std::cout << "line = " << line << std::endl;
-
 		// Add all digits to the vector
 		for (char c : line)
 			digits.push_back(c - '0');
@@ -69,7 +67,6 @@ int main(int argc, char **argv)
                     pow(10.0, static_cast<double>(MIN_LINE_SIZE - di - 1))
                     );
 			}
-			std::cout << "joltage = " << joltage << "\n\n";
 			total_joltage += joltage;
 			continue;
 		}
@@ -77,8 +74,6 @@ int main(int argc, char **argv)
 		it_cnt = 0;
 		while (it_cnt < digits.size())
 		{
-			std::cout << "\n#################### " << it_cnt + 1 << " ITERATION\n\n";
-
 			// Let's find the first maximum in the interval
 			// [cur_pos; digits.size() - (MIN_LINE_SIZE - chosen)]
 			max_pos = cur_pos;	
@@ -88,13 +83,9 @@ int main(int argc, char **argv)
 					max_pos = di;
 			}
 			max = digits[max_pos];
-			std::cout << "max = " << max << std::endl;
-			std::cout << "max_pos = " << max_pos << std::endl;
 
 			// Let's skip all digits in [cur_pos; max_pos)
 			skip += max_pos - cur_pos;
-			std::cout << "skip = max_pos - cur_pos = "
-				<< max_pos << " - " << cur_pos << " = " << skip << std::endl;
 
 			// Add found maximum digit to the current joltage
 			joltage += static_cast<size_t>(max)
@@ -102,32 +93,19 @@ int main(int argc, char **argv)
                 pow(10.0, static_cast<double>(MIN_LINE_SIZE - chosen - 1))
                 );
 
-			// Increment number of digits we 
 			++chosen;
-			std::cout << "chosen = " << chosen << std::endl;
 
 			// Update current position
 			cur_pos = max_pos + 1;
-			std::cout << "cur_pos = max_pos + 1 = "
-				<< max_pos << " + 1 = " << cur_pos << std::endl;
 
 			// Count how many digits remain to process
 			rest = digits.size() - skip - chosen;
-			std::cout << "rest = digits.size() - skip - chosen = "
-				<< digits.size() << " - " << skip << " - " << chosen
-				<< " = " << rest << std::endl;
 
 			if (chosen == MIN_LINE_SIZE)
-			{
-				std::cout << "chosen == MIN_LINE_SIZE\n";
-				std::cout << "break\n";
 				break;
-			}
 
 			if (chosen + rest == MIN_LINE_SIZE)
 			{
-				std::cout << "chosen + rest == MIN_LINE_SIZE\n";
-				std::cout << "Add all remaining digits\n";
 				for (int di = cur_pos; di < line.size(); ++di)
 				{
 					joltage += static_cast<size_t>(digits[di])
@@ -136,13 +114,11 @@ int main(int argc, char **argv)
                         );
 					++chosen;
 				}
-				std::cout << "break\n";
 				break;
 			}
 
 			++it_cnt;
 		}
-		std::cout << "joltage = " << joltage << "\n\n";
 		total_joltage += joltage;
     }
 	std::cout << "Total joltage = " << total_joltage << std::endl;
