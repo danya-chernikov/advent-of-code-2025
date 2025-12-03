0d88b5f47740f46b7a6aa1e7ab6ebd4bf14778ee
danya-chernikov <dchernik@proton.me>
2025-12-03 14:09:41 +0100

    The first part of the third day puzzle was solved

--- 3/part1/3_p1_debug.cpp
+++ 3/part1/3_p1_debug.cpp
@@ -34,6 +34,7 @@ int main(int argc, char **argv)
 		std::cout << line << std::endl;
 		std::vector<int> digits;
 
+		// Add all digits to the vector
 		for (char c : line)
 			digits.push_back(c - '0');
 		
@@ -47,6 +48,10 @@ int main(int argc, char **argv)
 		}
 		else
 		{
+			// Let's find the first maximum number first
+			// By 'first' we imply also, if for example
+			// we have: 4732893159
+			// the index of the msximum number will be 5
 			int	max_pos = 0;
 			int	max_pos2 = 0;
 			for (int i = 1; i < digits.size(); ++i)
@@ -54,30 +59,49 @@ int main(int argc, char **argv)
 				if (digits[max_pos] < digits[i])
 					max_pos = i;
 			}
+			std::cout << "\tmax_pos = " << max_pos << std::endl;
+			// If max_pos is the last index, we have
+			// to move left to find the next maximum
+			// It's guaranteed that in this case this
+			// maximum number is unique in all bank
 			if (max_pos == digits.size() - 1)
 			{
+				std::cout << "\tdigits[max_pos] is on the last index\n";
+				std::cout << "\tgoing to the left...\n";
 				max_pos2 = 0;
 				for (int i = 1; i < max_pos; ++i)
 				{
 					if (digits[max_pos2] < digits[i])
 						max_pos2 = i;
 				}
+				std::cout << "\tmax_pos2 = " << max_pos2 << std::endl;
 				res = (10 * digits[max_pos2]) + digits[max_pos];
 			}
+			// It's still the first founded maximum
+			// but it may have duplicates, if it has
+			// at least one duplicate it will be located
+			// somewhere on the right, in case if it hasn't
+			// any duplicate, we still just have to go on
+			// the right to find the next maximum
+			// Otherwise, we have to move right
 			else
 			{
+				std::cout << "\tdigits[max_pos] is somewhere is the middle\n";
+				std::cout << "\tgoing to the right...\n";
 				max_pos2 = max_pos + 1;
 				for (int i = max_pos + 2; i < digits.size(); ++i)
 				{
 					if (digits[max_pos2] < digits[i])
 						max_pos2 = i;
 				}
+				std::cout << "\tmax_pos2 = " << max_pos2 << std::endl;
 				res = (10 * digits[max_pos]) + digits[max_pos2];
 			}
 		}
+		std::cout << "\tres = " << res << std::endl;
 		sum += res;
     }
-	std::cout << "Total joltage is: " << sum << std::endl;
+	std::cout << "sum = " << sum << std::endl;
 
     in.close();
     return 0;
